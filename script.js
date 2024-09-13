25704954446490f46701350794f92b191d69c416
amutones <aurgena@gmail.com>
2024-09-13 10:18:49 -0500

    Game works to console as intended for script.js

--- script.js
+++ script.js
@@ -1,14 +1,90 @@
 // console.log("Hello World");
+let humanScore = 0;
+let computerScore = 0;
+let gamesPlayed = 0;
+
+playGame();
 
 function getComputerChoice() {
     let choice = Math.random();
     if (choice < 0.34) {
-        return "Rock";
+        return 1;
     }
     else if (choice < 0.67) {
-        return "Paper";
+        return 2;
     }
     else {
-        return "Scissors";
+        return 3;
+    }
+}
+
+function getHumanChoice() {
+    let humChoice = prompt("Rock, paper, or scissors?");
+    humChoice = humChoice.toUpperCase();
+    let humCompare = 0;
+    switch (humChoice) {
+        case "ROCK":
+            return 1;
+            break;
+        case "PAPER":
+            return 2;
+            break;
+        case "SCISSORS":
+            return 3;
+            break;
+        default:
+            break;
+    }
+}
+
+function playRound(humanChoice, computerChoice) {
+    console.log(humanChoice, computerChoice);
+    if (humanChoice == computerChoice) {
+        console.log("Draw. Go again");
+        return;
+    }
+    switch (humanChoice) {
+        case 1:
+            if (computerChoice == 2) {
+                computerScore++;
+            }
+            else {
+                humanScore++;
+            }
+            break;
+        case 2:
+            if (computerChoice == 1) {
+                humanScore++;
+            }
+            else {
+                computerScore++;
+            }
+            break;
+        case 3:
+            if (computerChoice == 1) {
+                computerScore++;
+            }
+            else {
+                humanScore++
+            }
+            break;
+        default:
+            break;
+    }
+    gamesPlayed++;
+}
+
+
+function playGame() {
+    while (gamesPlayed < 5) {
+        playRound(getHumanChoice(), getComputerChoice());
+        console.log("Score: " + humanScore + " " + computerScore);
+    }
+
+    if (humanScore > computerScore) {
+        console.log("Congrats! You won!")
+    }
+    else {
+        console.log("Sorry! You lost!")
     }
 }
