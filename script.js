696508c2ef05c2553aac9d57e3a55d12c85c3b1f
amutones <aurgena@gmail.com>
2024-09-26 14:13:42 -0500

    Created correctly working interface  On branch rps-ui  Changes to be committed: 	modified:   script.js

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-// console.log("Hello World");
-let humanScore = 0;
-let computerScore = 0;
-// let gamesPlayed = 0;
-let humanChoice = "";
+let humanScoreCount = 0;
+let computerScoreCount = 0;
 const GAMES_TO_WIN = 3;
-const div = document.createElement("div");
+const scoreContainer = document.querySelector("#score-container");
+const selectionContainer = document.querySelector("#selection-container");
 const buttons = document.querySelectorAll("button");
-// playGame();
+const roundResult = document.querySelector("#round-result");
+const humanSelection = document.querySelector("#human-selection");
+const computerSelection = document.querySelector("#computer-selection");
+const final = document.querySelector("#final");
+
 
 buttons.forEach((button) => {
     
         button.addEventListener("click", () => {
             playGame(button.id);
-            // playRound(getHumanChoice(button.id),getComputerChoice());
+            
         });  
  
 });
@@ -21,28 +23,32 @@ buttons.forEach((button) => {
 function getComputerChoice() {
     let choice = Math.random();
     if (choice < 0.34) {
+        computerSelection.textContent = "Computer Choice: Rock";
         return 1;
     }
     else if (choice < 0.67) {
+        computerSelection.textContent = "Computer Choice: Paper";
         return 2;
     }
     else {
+        computerSelection.textContent = "Computer Choice: Scissors";
         return 3;
     }
 };
 
 function getHumanChoice(humanChoice) {
-    //let humanChoice = prompt("Rock, paper, or scissors?");
     console.log(humanChoice);
-    //humanChoice = humanChoice.toUpperCase();
     switch (humanChoice) {
         case "rock-button":
+            humanSelection.textContent = "Your Choice: Rock";
             return 1;
             break;
         case "paper-button":
+            humanSelection.textContent = "Your Choice: Paper";
             return 2;
             break;
         case "scissors-button":
+            humanSelection.textContent = "Your Choice: Scissors";
             return 3;
             break;
         default:
@@ -52,33 +58,35 @@ function getHumanChoice(humanChoice) {
 
 function playRound(humanChoice, computerChoice) {
     console.log(humanChoice, computerChoice);
+    roundResult.textContent = "";
     if (humanChoice == computerChoice) {
+        roundResult.textContent = "Draw. Go again.";
         console.log("Draw. Go again");
         return;
     }
     switch (humanChoice) {
         case 1:
             if (computerChoice == 2) {
-                computerScore++;
+                computerScoreCount++;
             }
             else {
-                humanScore++;
+                humanScoreCount++;
             }
             break;
         case 2:
             if (computerChoice == 1) {
-                humanScore++;
+                humanScoreCount++;
             }
             else {
-                computerScore++;
+                computerScoreCount++;
             }
             break;
         case 3:
             if (computerChoice == 1) {
-                computerScore++;
+                computerScoreCount++;
             }
             else {
-                humanScore++
+                humanScoreCount++
             }
             break;
         default:
@@ -89,22 +97,29 @@ function playRound(humanChoice, computerChoice) {
 
 
 function playGame(choice) {
-    
     playRound(getHumanChoice(choice), getComputerChoice());
-    console.log("Score: " + humanScore + " " + computerScore);
+    const humanScore = document.querySelector("#human-score");
+    const computerScore = document.querySelector("#computer-score");
+    humanScore.textContent = "Your score: " + humanScoreCount;
+    computerScore.textContent = "Computer score: " + computerScoreCount;
+    
+    // console.log("Score: " + humanScore + " " + computerScore);
     
 
     
-    if (humanScore === GAMES_TO_WIN) {
-        console.log("Congrats! You won!")
-        humanScore = 0;
-        computerScore = 0;
+    if (humanScoreCount === GAMES_TO_WIN) {
+        
+        final.textContent = "Congrats! You won!";
+        // console.log("Congrats! You won!")
+        humanScoreCount = 0;
+        computerScoreCount = 0;
     }
     
-    if (computerScore === GAMES_TO_WIN){
-        console.log("Sorry! You lost!")
-        humanScore = 0;
-        computerScore = 0;
+    if (computerScoreCount === GAMES_TO_WIN){
+        final.textContent = "Sorry! You lost!";
+        // console.log("Sorry! You lost!")
+        humanScoreCount = 0;
+        computerScoreCount = 0;
     }
 
 }
